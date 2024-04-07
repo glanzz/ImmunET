d764db39cf4c591ce6e212c428d427a7ee5b3b02
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-04-07 14:28:43 -0400

    Update and move pets

--- src/main/java/com/immunet/immunet/entity/Pet.java
+++ src/main/java/com/immunet/immunet/entity/Pet.java
@@ -1,8 +1,12 @@
-package com.immunet.immunet.model;
+package com.immunet.immunet.entity;
 import java.util.Date;
+
+import org.springframework.lang.NonNull;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,26 +15,38 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="pets")
-public class Pet {
+public class Pet extends BaseEntity {
 	@Id
+	@NonNull
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	
-	@Column(name="name")
-	private String name;
 
+	@NonNull
+	@Column(name="name", nullable=false)
+	private String name;
+	
+	@Column(nullable=false)
 	private Date dob;
-	private String gender;
-	private String species;
-	public Pet(String name, Date dob, String gender, String species) {
+	
+	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+	
+	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
+	private Species species;
+
+	public Pet(String name, Date dob, Gender gender, Species species) {
 		this.name = name;
 		this.dob = dob;
 		this.gender = gender;
 		this.species = species;
 	}
+	
 	public Integer getId() {
 		return id;
 	}
+	
 	public String getName() {
 		return name;
 	}
@@ -43,16 +59,16 @@ public class Pet {
 	public void setDob(Date dob) {
 		this.dob = dob;
 	}
-	public String getGender() {
+	public Gender getGender() {
 		return gender;
 	}
-	public void setGender(String gender) {
+	public void setGender(Gender gender) {
 		this.gender = gender;
 	}
-	public String getSpecies() {
+	public Species getSpecies() {
 		return species;
 	}
-	public void setSpecies(String species) {
+	public void setSpecies(Species species) {
 		this.species = species;
 	}
 	
--- src/main/java/com/immunet/immunet/repository/PetRepository.java
+++ src/main/java/com/immunet/immunet/repository/PetRepository.java
@@ -2,7 +2,7 @@ package com.immunet.immunet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.immunet.immunet.model.Pet;
+import com.immunet.immunet.entity.Pet;
 
 
 public interface PetRepository extends JpaRepository<Pet, Integer>{
