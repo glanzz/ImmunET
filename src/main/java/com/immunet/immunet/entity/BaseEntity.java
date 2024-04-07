1261c4b5d19d1d5aa7d5b07d8a5a6d6ca3687fa3
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-04-07 14:59:20 -0400

    Fix: BaseEntity to be considered as valid super class

--- src/main/java/com/immunet/immunet/entity/BaseEntity.java
+++ src/main/java/com/immunet/immunet/entity/BaseEntity.java
@@ -4,11 +4,22 @@ package com.immunet.immunet.entity;
 import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.lang.NonNull;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 
+
+@MappedSuperclass
 public class BaseEntity {
+	@Id
+	@NonNull
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
 	
 	@Column(name = "created_at", nullable=false, updatable = false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     @CreationTimestamp
@@ -18,6 +29,10 @@ public class BaseEntity {
 	@PrePersist
 	protected void onCreate() {
 	  setCreatedAt(new Date());
+	}
+	
+	public Integer getId() {
+		return id;
 	}
 
 	public Date getCreatedAt() {
