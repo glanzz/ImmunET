23d1269f5230bf4926753cea57638c36ec620406
Hitesh Krishnappa <krishnappa.h@northeastern.edu>
2024-04-07 23:34:20 -0400

    Add bill entity

--- src/main/java/com/immunet/immunet/entity/Bill.java
+++ src/main/java/com/immunet/immunet/entity/Bill.java
@@ -1,5 +1,43 @@
 package com.immunet.immunet.entity;
 
-public class Bill {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="bills")
+public class Bill extends BaseEntity {
+
+	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
+	private Status status = Status.PENDING;
+	
+	@Column(nullable=false)
+	private Double taxPercent;
+	
+	public Bill(Status status, Double taxPercent) {
+		super();
+		this.status = status;
+		this.taxPercent = taxPercent;
+	}
+	
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Double getTaxPercent() {
+		return taxPercent;
+	}
+
+	public void setTaxPercent(Double taxPercent) {
+		this.taxPercent = taxPercent;
+	}
+
 
 }
