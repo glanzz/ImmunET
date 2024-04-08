eb895e34f24f1ee2b6d6da1a32e7282f8fec887a
Hitesh Krishnappa <krishnappa.h@northeastern.edu>
2024-04-08 01:03:02 -0400

    Added bill entity

--- src/main/java/com/immunet/immunet/entity/Bill.java
+++ src/main/java/com/immunet/immunet/entity/Bill.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +19,14 @@ public class Bill extends BaseEntity {
 	
 	@Column(nullable=false)
 	private Double taxPercent;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bill_for", referencedColumnName = "id", nullable=false)
+    private Pet billFor;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bill_by", referencedColumnName = "id", nullable=false)
+    private User billBy;
 	
 	public Bill(Status status, Double taxPercent) {
 		super();
@@ -37,6 +48,22 @@ public class Bill extends BaseEntity {
 
 	public void setTaxPercent(Double taxPercent) {
 		this.taxPercent = taxPercent;
+	}
+
+	public Pet getBillFor() {
+		return billFor;
+	}
+
+	public void setBillFor(Pet pet) {
+		this.billFor = pet;
+	}
+
+	public User getBillBy() {
+		return billBy;
+	}
+
+	public void setCreator(User creator) {
+		this.billBy = creator;
 	}
 
 
